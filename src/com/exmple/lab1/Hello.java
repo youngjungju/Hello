76759d75b0d490622cbcf8dcd82f04746cf4a1e2
youngjungju <22000694@handong.edu>
2021-09-03 11:20:53 +0900

    1st commit

--- src/com/exmple/lab1/Hello.java
+++ src/com/exmple/lab1/Hello.java
@@ -1,6 +1,10 @@
 package com.exmple.lab1;
 
 public class Hello {
-	System.out.println("Hello World!");
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+	
+	
 
 }
